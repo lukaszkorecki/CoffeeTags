540fc0f1be67a4099de31c92721e033e0dabbd1a
Masatake YAMATO <yamato@redhat.com>
2014-08-17 16:06:15 +0900

    update the expected output of test cases
    * s/lineno:/line:/
    * s/type:/kind:/
    * append language:coffee
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -14,14 +14,14 @@ describe 'CoffeeTags::Formatter' do
 
   it "generates a line for method definition" do
     exp = [ 'constructor', 'test.coffee', "/^  constructor: (api_key, host) ->$/;\"",
-            'f', 'lineno:8', 'object:Campfire', 'type:function'
+            'f', 'line:8', 'object:Campfire', 'kind:function', 'language:coffee',
           ].join("\t")
     @instance.parse_tree.first.should == exp
   end
 
   it "generates line for second class" do
     exp = [ 'bump', 'test.coffee', "/^  bump : ->$/;\"",
-            'f', 'lineno:46', 'object:Test', 'type:function'
+            'f', 'line:46', 'object:Test', 'kind:function', 'language:coffee',
           ].join "\t"
     @instance.parse_tree.last.should == exp
   end
